3a01ec30cc4bb00735963b96c5c9d7579705221e
FelipeNavasTodoo <ln@todoo.co>
2021-03-04 14:48:43 -0500

    prueba

---
+++ reports_account_thomas/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name': 'Reports account thomas',
+
+    'version': '13.1',
+
+    'author': "Todoo SAS",
+
+    'contributors': ['Luis Felipe Navas ln@todoo.co'],
+
+    'website': "www.todoo.co",
+
+    'category': 'Reports',
+
+    'depends': [
+
+        'account_accountant',
+        'account',
+
+    ],
+
+    'data': [       
+        'reports/report_advances.xml',                   
+        'reports/report_cash.xml',                   
+        'reports/report_credit_note.xml',                   
+        'reports/report_discharge.xml',                   
+        'reports/report_payroll_discounts.xml',                   
+    ],
+    'installable': True
+}
