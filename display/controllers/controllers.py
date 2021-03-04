e9dec4b5897516b5de77009962b05683c29770f9
FelipeNavasTodoo <ln@todoo.co>
2021-03-04 12:44:48 -0500

    module scaffold

---
+++ display/controllers/controllers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# from odoo import http
+
+
+# class ../user/display(http.Controller):
+#     @http.route('/../user/display/../user/display/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/../user/display/../user/display/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('../user/display.listing', {
+#             'root': '/../user/display/../user/display',
+#             'objects': http.request.env['../user/display.../user/display'].search([]),
+#         })
+
+#     @http.route('/../user/display/../user/display/objects/<model("../user/display.../user/display"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('../user/display.object', {
+#             'object': obj
+#         })
